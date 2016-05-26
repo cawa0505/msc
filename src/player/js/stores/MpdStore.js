f54149020ff6f038873cdaddfb231732745c2205
Jimmy Yen <zeng.tw@gmail.com>
2016-05-27 07:39:31 +0800

    nothing

--- src/player/js/stores/MpdStore.js
+++ src/player/js/stores/MpdStore.js
@@ -4,7 +4,7 @@ var MscActions    = require('../actions/MscActions');
 var EventEmitter  = require('events').EventEmitter;
 var assign        = require('object-assign');
 var SettingsStore = require('./SettingsStore');
-var ipc           = require('ipc');
+var ipc 					= require('electron').ipcMain;
 
 var CHANGE_EVENT = 'change';
 
@@ -39,52 +39,52 @@ ipc.on('status-update', function (data) {
 var MpdStore = assign({}, EventEmitter.prototype, {
 
   connect: function () {
-   ipc.send('connect');
+    ipc.send('connect');
   },
 
   getStatus: function () {
-   return status;
+    return status;
   },
 
   emitChange: function () {
-   this.emit(CHANGE_EVENT);
+    this.emit(CHANGE_EVENT);
   },
 
-	addChangeListener: function (callback) {
-		this.on(CHANGE_EVENT, callback);
-	},
+  addChangeListener: function (callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
 
-	removeChangeListener: function (callback) {
-		this.removeListener(CHANGE_EVENT, callback);
-	},
+  removeChangeListener: function (callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  },
 
-	dispatcherIndex: AppDispatcher.register(function (payload) {
-		switch (payload.actionType) {
-			case Constants.MPD_CONNECT:
-				ipc.send('connect');
-				break;
-			case Constants.MPD_TOGGLE_PLAYBACK:
-				ipc.send('toggle-playback');
-				break;
-			case Constants.MPD_PREV:
-				ipc.send('prev-song');
-				break;
-			case Constants.MPD_NEXT:
-				ipc.send('next-song');
-				break;
-			case Constants.MPD_SEEK:
-				var percent = payload.data.percent;
-				if (percent <= 0.015) percent = 0;
-				ipc.send('seek', percent * status.Duration);
-				break;
-			case Constants.MPD_REPEAT:
-				ipc.send('repeat', (status.Repeat ? '0' : '1'));
-				break;
-			case Constants.MPD_RANDOM:
-				ipc.send('random', (status.Random ? '0' : '1'));
-				break;
-		}
-	})
+  dispatcherIndex: AppDispatcher.register(function (payload) {
+    switch (payload.actionType) {
+    case Constants.MPD_CONNECT:
+      ipc.send('connect');
+    break;
+    case Constants.MPD_TOGGLE_PLAYBACK:
+      ipc.send('toggle-playback');
+    break;
+    case Constants.MPD_PREV:
+      ipc.send('prev-song');
+    break;
+    case Constants.MPD_NEXT:
+      ipc.send('next-song');
+    break;
+    case Constants.MPD_SEEK:
+      var percent = payload.data.percent;
+      if (percent <= 0.015) percent = 0;
+      ipc.send('seek', percent * status.Duration);
+    break;
+    case Constants.MPD_REPEAT:
+     ipc.send('repeat', (status.Repeat ? '0' : '1'));
+     break;
+    case Constants.MPD_RANDOM:
+     ipc.send('random', (status.Random ? '0' : '1'));
+     break;
+  }
+  }),
 });
 
 module.exports = MpdStore;
